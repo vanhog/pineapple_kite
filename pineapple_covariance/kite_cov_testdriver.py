5dccfa04aa62f9737db8abdd6de21fcbb2b61e90
vanhog <dieter@hoogestraat.dk>
2024-10-19 08:11:07 +0200

    align kite data with grond setup - but blacklisting must earlier

--- pineapple_covariance/kite_cov_testdriver.py
+++ pineapple_covariance/kite_cov_testdriver.py
@@ -1,14 +1,20 @@
 from kite import Scene
 import matplotlib.pyplot as plt
 import numpy as np 
+import os
 from matplotlib import cm, colors
 
 source_scene = Scene.load('/media/hog/docCrucial1T/msc_grond_noemi/volume_opti/data/events/roenne/insar_float32_losturn/tl5_l2b_a_044_01_mscaoi_f32_foccov_losturn.npz')
+source_scene = Scene.load('/home/hog/data/mscthesis/insar_float32_losturn/tl5_l2b_a_117_02_mscaoi_f32_foccov_losturn.npz')
 noisecoords = source_scene.covariance.noise_coord
 sourceeps = source_scene.quadtree.epsilon
 sourcenan = source_scene.quadtree.nan_allowed
 sourcemintile = source_scene.quadtree.tile_size_min
 sourcemaxtile = source_scene.quadtree.tile_size_max
+sourcetitle = source_scene.meta.scene_title
+sourceID = source_scene.meta.scene_id
+sourceorbit = source_scene.meta.orbital_node
+sourcesatellite = source_scene.meta.satellite_name
 #sourceblack   = source_scene.quadtree.config.leaf_blacklist
 sourcepolygm  = source_scene.config.polygon_mask
 print('noisecoords:\t', noisecoords)
@@ -17,13 +23,23 @@ print('nan: \t', sourcenan)
 print('tilemin:\t', sourcemintile)
 print('tilemax:\t', sourcemaxtile)
 print('polyonm:\t', sourcepolygm)
-target_scene = Scene.load('/home/hog/data/dev/testdata/kite_qt_scene.npz')
+print('Satellite:\t', sourcesatellite)
+print('Orbit:\t', sourceorbit)
+target_scene = Scene.load('/home/hog/Downloads/tl5_a_117_02_mscaoi_sent1ABmv.npz')
 target_scene.config.polygon_mask = sourcepolygm
 target_scene.quadtree.epsilon = sourceeps
 target_scene.quadtree.nan_allowed = sourcenan
 target_scene.quadtree.tile_size_min = sourcemintile
 target_scene.quadtree.tile_size_max = sourcemaxtile
+target_scene.meta.scene_title = sourcetitle
+target_scene.meta.scene_id = sourceID
+target_scene.meta.orbital_node = sourceorbit 
+target_scene.meta.satellite = sourcesatellite
 #target_scene.quadtree.config.leaf_blacklist = sourceblack
 target_scene.covariance.noise_coord = noisecoords
-target_scene.save('/home/hog/data/dev/testdata/kite_qt_scene_com.npz')
+covfull = target_scene.covariance.covariance_matrix
+out_filename = os.path.basename(os.path.realpath(source_scene.meta.filename))
+out_dir = '/home/hog/Downloads/'
+out_path = out_dir + out_filename
+target_scene.save(out_path)
 print('happy days')
